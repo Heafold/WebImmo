6e18f986a8689fa662df2f5c8d729b02245a1ec7
Heafold <mcorentin62@gmail.com>
2022-09-15 17:52:36 +0200

    show bdd

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/parcourir', [PropertyController::class, 'index'])->name('show');
